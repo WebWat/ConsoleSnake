f29168a378485c8d9a9c182f54ccf8e904457f43
Serega <seregaplyaskin8@gmail.com>
2021-11-10 20:13:39 +0300

    apples

--- ConsoleSnake/GameSettings.cs
+++ ConsoleSnake/GameSettings.cs
@@ -3,7 +3,7 @@
     public class GameSettings
     {
         public const int MapSize = 35 * PixelSize;
-        public const int Speed = 150;
+        public const int Speed = 100;
         public const int PixelSize = 3;
         public const int MapSizeInPixels = (MapSize / PixelSize) - PixelSize;
     }
--- ConsoleSnake/Program.cs
+++ ConsoleSnake/Program.cs
@@ -12,6 +12,9 @@ namespace ConsoleSnake
         private static bool GameOver = false;
         private static (int Left, int Top) Position = (1, 1);
         private static Queue<(int, int)> Coords = new();
+        private static Random Random = new Random();
+        private static List<(int, int)> ApplesCoords = new();
+        private static int SnakeLength = 1;
 
         private static Thread Game = new Thread(new ThreadStart(Move));
         private static Thread Launcher = new Thread(new ThreadStart(Launch));
@@ -46,6 +49,10 @@ namespace ConsoleSnake
 
             RenderPosition();
 
+            GenerateRandomAppleCoords(20);
+
+            RenderApples();
+
             while (true)
             {
                 var keyInfo = Console.ReadKey();
@@ -78,8 +85,11 @@ namespace ConsoleSnake
 
         private static void RenderPosition()
         {
-            if (Coords.TryDequeue(out (int, int) result))
-                ClearPixel(result);
+            if (Coords.Count == SnakeLength)
+            {
+                if (Coords.TryDequeue(out (int, int) result))
+                    ClearPixel(result);
+            }
 
             var worm = new Pixel(Position.Left, Position.Top, '#');
 
@@ -115,6 +125,12 @@ namespace ConsoleSnake
                 if (!IsValidPosition())
                     break;
 
+                if (IsApplePosition())
+                {
+                    Coords.Enqueue(Position);
+                    SnakeLength++;
+                }
+
                 RenderPosition();
 
                 Thread.Sleep(GameSettings.Speed);
@@ -128,6 +144,41 @@ namespace ConsoleSnake
         {
             return (Position.Top < GameSettings.MapSizeInPixels && Position.Top > 0) &&
                    (Position.Left < GameSettings.MapSizeInPixels && Position.Left > 0);
+        }
+
+
+        private static bool IsApplePosition()
+        {
+            var result = ApplesCoords.Any(i => i.Item1 == Position.Left && i.Item2 == Position.Top);
+
+            ApplesCoords.Remove(Position);
+
+            return result;
+        }
+
+
+        private static void GenerateRandomAppleCoords(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int top = Random.Next(2, GameSettings.MapSizeInPixels);
+                int left = Random.Next(2, GameSettings.MapSizeInPixels);
+
+                ApplesCoords.Add((left, top));
+            }
+        }
+
+
+        private static void RenderApples()
+        {
+            char wallSymbol = '@';
+
+            foreach (var item in ApplesCoords)
+            {
+                var pixel = new Pixel(item.Item1, item.Item2, wallSymbol);
+
+                pixel.Draw();
+            }
         }
 
 
